c5fe14e6bdaa7946a15a822a7dffc74eedd59447
guanguoxiang <abcgg8@qq.com>
2017-09-20 22:57:32 +0800

    Initial commit

---
+++ api/src/main/java/com/ggx/api/MethodCallback.java
@@ -0,0 +1,11 @@
+package com.ggx.api;
+
+/**
+ * Created by jerry.guan on 4/21/2017.
+ */
+
+public interface MethodCallback<T> {
+
+    void invoke(T source, int requestCode);
+    void invokeFail(T source, int requesCode);
+}
